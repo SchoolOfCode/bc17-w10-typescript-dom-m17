50aa74be2f5ec06979021e299e36403856207685
dom-m17 <dominicmaynard00@gmail.com>
2024-09-07 17:16:09 +0300

    task 3 v1

---
+++ task-3/src/app.ts
@@ -0,0 +1,27 @@
+function getNewJoke() {
+  fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      "Accept": "application/json"  // Explicitly request a JSON response
+    }
+  })
+  .then((response) => {
+    return response.json();
+  })
+  .then(data => {
+    const quoteDisplay = document.getElementById("quote-display");
+  
+    if (quoteDisplay) {
+      quoteDisplay.innerHTML = data.joke;
+    } else {
+      console.error('Element with id "quote-display" not found.');
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+}
+
+const button = document.getElementById("get-quote")
+if (button) {
+  button.addEventListener('click', getNewJoke)
+}
